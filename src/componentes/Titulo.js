9803901d582ce112e593692fb57777aff255e598
Carlos Jose Pereira <carlaopereirasanca@gmail.com>
2024-08-26 16:31:03 -0300

    commit inicial

---
+++ src/componentes/Titulo.js
@@ -0,0 +1,13 @@
+
+function Titulo() {
+
+    return (
+      
+      <div>
+        <h1>IFSP</h1>
+      </div>
+    );
+  
+  }
+  
+  export default Titulo;
---
+++ src/componentes/Titulo2.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+
+class Titulo2 extends React.Component {
+
+    render() {
+
+      return (
+        <div>
+          <h1>IFSP</h1>
+        </div>
+      );
+
+    }
+    
+  }
+  
+  export default Titulo2;
